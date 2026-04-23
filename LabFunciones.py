c7fe1e1a8708b9aa9740b53d45753fc9dc853d6a
Becsy yomar Sandoval Villanueva <becsysandoval@gmail.com>
2026-04-23 11:26:00 -0500

    Becsy Sandoval - Funciones matemáticas

--- LabFunciones.py
+++ LabFunciones.py
@@ -4,3 +4,22 @@ def saludar(nombre):
 
 saludar("Frank")
 saludar("Ana")
+
+def sumar(a, b):
+    return a + b
+
+# Función 2: multiplicar
+def multiplicar(a, b):
+    return a * b
+
+# Función 3: es_par
+def es_par(numero):
+    if numero % 2 == 0:
+        return "Par"
+    else:
+        return "Impar"
+    
+print(sumar(3, 5))
+print(multiplicar(4, 6))
+print(es_par(4))
+print(es_par(7))
